fa19848bf047721b067b3a36276c1baf3cadaf0c
ericminnick <minnick.eric@gmail.com>
2026-01-13 16:02:36 -0600

    CH2L5

--- handler_user.go
+++ handler_user.go
@@ -9,6 +9,20 @@ import (
 	"github.com/google/uuid"
 )
 
+func handlerUsers(s *state, cmd command) error {
+	if len(cmd.Args) > 1 {
+		return fmt.Errorf("Usage: %s", cmd.Name)
+	}
+	
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't find any users: %w", err)
+	}
+	printUsers(users, s)
+	return nil
+	
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("Usage: %s <name>", cmd.Name)
@@ -58,6 +72,16 @@ func handlerRegister(s *state, cmd command) error {
 func printUser(user database.User) {
 	fmt.Printf(" * ID:		%v\n", user.ID)
 	fmt.Printf(" * Name:	%v\n", user.Name)
+}
+
+func printUsers(users []string, s *state) {
+	for _, user := range users {
+		if s.cfg.CurrentUserName == user {
+			fmt.Printf("* %v (current)\n", user)
+		} else {
+			fmt.Printf("* %v\n", user)
+		}
+	}
 }
 
 func handlerReset(s *state, cmd command) error {
